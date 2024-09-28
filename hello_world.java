9cf3d9ca77e9ae7352125e153d54e2a3c4bd1e8b
Shiva Subramanyam <107316558+Shiva-ml@users.noreply.github.com>
2024-09-28 19:55:31 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World, HI there poll!");
+        System.out.println("Hello, World, HI there webhook!");
     }
 }
